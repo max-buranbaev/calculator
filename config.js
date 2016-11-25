3fadd45b85cf9845c3a5e5372462209e1adcae66
Max Buranbaev <max@izst.ru>
2016-11-25 12:20:35 +0100

    add mongolabdb

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 
 exports.port = process.env.PORT || 4000;
 exports.mongodb = {
-    uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/test1'
+    uri: 'mongodb://heroku_fwh6p668:sajl7va1071dtmlqsd962mm3nr@ds163667.mlab.com:63667/heroku_fwh6p668'
 };
