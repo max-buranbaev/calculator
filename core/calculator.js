4934f5f3e48dde4ebccd3b9db678ace07fafe592
Max Buranbaev <max@izst.ru>
2016-11-25 11:49:40 +0100

    adding get query

--- core/calculator.js
+++ core/calculator.js
@@ -16,3 +16,12 @@ exports.save = (req, res, next) => {
     });
 
 };
+
+exports.getAll = (req, res, next) => {
+    const db = req.app.db;
+    const Calculation = db.model('Calculation');
+    Calculation.find().select({calculation: 1, date: 1, result: 1}).exec((err, result) => {
+        if(err) next(err);
+        res.status(200).send(result);
+    })
+};
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ exports = module.exports = app => {
     app.use((req, res, next) => next());
 
     app.post('/calculations', require('../core/calculator').save);
+    app.get('/calculations', require('../core/calculator').getAll);
 
     app.get('/', (req, res, next) => res.render('../public/index.html'));
 
